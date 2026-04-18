a8b6e108cc9463f73a32a8acacddd93237e04414
Purnet Moirangthem <purnet.moirangthem@kapturecrm.com>
2026-04-18 16:57:44 +0530

    fix: suppress body hydration warning from browser extension

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       className={`${dmSerifDisplay.variable} ${dmSans.variable}`}
       suppressHydrationWarning
     >
-      <body>
+      <body suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
